6ce2eb35c89393675a8bed89dd25f9d49a1e60ef
Jakub Perzylo <jakub.perzylo@swmansion.com>
2020-08-28 11:39:00 +0200

    Work in progress

--- lib/adapters/gun.ex
+++ lib/adapters/gun.ex
@@ -1,87 +1,122 @@
-defmodule Janus.Transport.WS.Adapters.Gun do
-  use Janus.Transport.WS.Adapter
-  use GenServer
+if Code.ensure_loaded?(WebSockex) do
+  defmodule Janus.Transport.WS.Adapters.Gun do
+    use GenServer
+    use Janus.Transport.WS.Adapter
+    alias Janus.Transport.WS.Adapter
 
-  @impl true
-  def connect(url, receiver, opts) do
-    timeout = opts[:timeout] || 5000
-    args = %{
-      message_receiver: receiver,
-      notify_on_connect: self(),
-      timeout: timeout
-    }
+    @impl Adapter
+    def connect(url, receiver, opts) do
+      timeout = opts[:timeout] || 5000
+      extra_headers = opts[:extra_headers] || []
 
-    case start_link(url, args) do
-      {:ok, ws} ->
+      args = %{
+        message_receiver: receiver,
+        extra_headers: extra_headers
+      }
+
+      start_link(url, timeout, args)
+    end
+
+    @impl Adapter
+    def send(client, frame) do
+      GenServer.cast(client, {:send, frame})
+    end
+
+    @impl Adapter
+    def disconnect(client) do
+      GenServer.cast(client, :disconnect)
+    end
+
+    def start_link(url, timeout, args), do: do_start(:start_link, url, timeout, args)
+
+    def start(url, timeout, args), do: do_start(:start, url, timeout, args)
+
+    defp do_start(method, url, timeout, args) do
+      apply(GenServer, method, [__MODULE__, {url, timeout, args}, []])
+    end
+
+    @impl GenServer
+    def init({url, timeout, args}) do
+      with [_protocol, _host, _port] = conn_params <- parse_url(url) do
+        case create_ws_connection(conn_params, timeout, args) do
+          {:connected, conn} ->
+            {:ok, %{connection: conn, message_receiver: args[:message_receiver]}}
+
+          {:error, reason} ->
+            {:stop, reason}
+        end
+      else
+        {:error, reason} ->
+          {:stop, reason}
+      end
+    end
+
+    @impl GenServer
+    def handle_cast(:disconnect, %{connection: conn, message_receiver: receiver} = state) do
+      :ok = :gun.close(conn)
+      notify_status(receiver, {:disconnected, "disconnected on request"})
+      {:stop, state}
+    end
+
+    def handle_cast({:send, frame}, %{connection: conn} = state) do
+      :ok = :gun.ws_send(conn, frame)
+      {:noreply, state}
+    end
+
+    @impl GenServer
+    def handle_info({:gun_ws, _conn, _stream_ref, frame}, %{message_receiver: receiver} = state) do
+      forward_frame(receiver, frame)
+      {:noreply, state}
+    end
+
+    def handle_info(
+          {:DOWN, _ref, :process, conn, reason},
+          %{connection: connection, message_receiver: receiver} = state
+        )
+        when conn == connection do
+      notify_status(receiver, {:disconnected, reason})
+      {:stop, {:connection, reason}, state}
+    end
+
+    # ignore protocol and try to connect without tsl
+    defp create_ws_connection([_protocol, host, port], timeout, %{extra_headers: extra_headers}) do
+      with {:ok, conn} <- :gun.open(String.to_charlist(host), port, %{connect_timeout: timeout}) do
+        IO.puts("CONNECTING")
+        Process.monitor(conn)
+        {:ok, _protocol} = :gun.await_up(conn)
+
+        :gun.ws_upgrade(conn, "/", extra_headers)
+        IO.puts("UPGRADING")
 
         receive do
-          {:connected, _connection} ->
-            {:ok, ws}
+          {:gun_upgrade, conn, _stream_ref, [<<"websocket">>], _headers} ->
+            {:connected, conn}
 
-        after
-          timeout ->
-            {:error, :connection_timeout}
+          {:gun_response, _conn, _, _, _status, _} ->
+            {:error, :upgrade_failed}
+
+          {:gun_error, _conn, _, reason} ->
+            {:error, reason}
+
+          error ->
+            {:error, error}
         end
-
-
-
-      {:error, _} = error ->
-        error
-
-    end
-
-  end
-
-  def start_link(url, args) do
-    nil
-  end
-
-  @impl true
-  def init({url, receiver, timeout}) do
-    result = case parse_url(url) do
-      {:error, _reason} = error ->
-        error
-      [protocol, host, port] = conn_params ->
-        case create_ws_connection(conn_params, timeout) do
-
-        end
-
-    end
-  end
-
-
-  # ignore protocol and try to connecct without tsl
-  defp create_ws_connection([_protocol, host, port], timeout) do
-    with {:ok, conn} = :gun.open(host, port) do
-      {:ok, _protocol} = :gun.await_up(conn)
-
-      :gun.ws_upgrade(conn, "/", [
-        {<<"Sec-WebSocket-Protocol">>, "janus-protocol"}
-      ])
-
-      receive do
-        {:gun_upgrade, conn, _stream_ref, [<<"websocket">>], _headers} ->
-          {:connected, conn}
-        {:gun_response, _conn, _, _, _status, _} ->
-          {:error, :upgrade_failed}
-        {:gun_error, _conn, _, reason} ->
-          {:error, reason}
-      after
-        timeout ->
-          {:error, :connection_timeout}
+      else
+        {:error, _} = error ->
+          IO.puts("ERROR CONNECTING")
+          error
       end
-    else
-      {:error, _} = error -> error
+    end
+
+    defp parse_url(url) do
+      case Regex.run(~r/(ws|wss):\/\/(.+):([0-9]+)/, url) do
+        nil ->
+          {:error, :invalid_url}
+
+        [_, protocol, host, port] ->
+          {port, _} = Integer.parse(port)
+          [protocol, host, port]
+      end
     end
   end
-
-  defp parse_url(url) do
-    case Regex.run(~r/(ws|wss):\/\/(.+):([0-9]+)/, url) do
-      nil -> {:error, :invalid_url}
-      [protocol, host, port] ->
-        {port, _} = Integer.parse(port)
-        [protocol, host, port]
-    end
-  end
-
 end
---
+++ test/adapters/gun_test.exs
@@ -0,0 +1,90 @@
+defmodule Janus.Transport.WS.Adapters.GunTest do
+  use ExUnit.Case
+
+  alias Janus.Transport.WS.Adapters
+  alias Janus.Transport.WS.Adapters.Gun
+
+  @url TestWebSocket.Server.get_url()
+
+  setup_all do
+    Application.ensure_all_started(:gun)
+    TestWebSocket.ClientConnection.start_link()
+
+    {:ok, server} = TestWebSocket.Server.start()
+
+    on_exit(fn ->
+      TestWebSocket.Server.shutdown()
+    end)
+
+    %{server: server}
+  end
+
+  # testing gun adapter against cowboy websocket server
+  describe "gun adapter should" do
+    setup do
+      {:ok, connection} = Gun.connect(@url, self(), [])
+
+      %{connection: connection}
+    end
+
+    test "connect with working remote server" do
+      assert {:ok, connection} = Gun.connect(@url, self(), [])
+    end
+
+    test "return error on invalid url" do
+      assert {:error, :test} = Gun.connect("invalid_url", self(), [])
+    end
+
+    test "return error on connection failure" do
+      assert {:error, :test} = Gun.connect("ws://no_server", self(), [])
+    end
+
+    test "disconnect on demand", %{connection: connection} do
+      Gun.disconnect(connection)
+
+      assert_receive {:disconnected, _}
+    end
+
+    test "send message to remote echo server and get it back", %{connection: connection} do
+      :ok = Gun.send(connection, "hey")
+
+      assert_receive {:ws_frame, "hey"}
+    end
+
+    test "send disconnect message on connection end" do
+      client = TestWebSocket.ClientConnection.get()
+      send(client, :stop)
+
+      assert_receive {:disconnected, _}
+    end
+  end
+
+  describe "Janus.Transport.WS when used with Gun should" do
+    alias Janus.Transport.WS
+
+    setup do
+      {:ok, {:state, _connection, _} = state} = WS.connect({@url, Adapters.Gun, []})
+      %{state: state}
+    end
+
+    test "stop on adapter disconnected message" do
+      msg = {:disconnected, "disconnect"}
+      {:stop, {:disconnected, _}, _} = WS.handle_info(msg, %{})
+    end
+
+    test "send and receive back message from adapter", %{state: state} do
+      payload = %{"message" => "hello"}
+
+      assert {:ok, state} = WS.send(payload, 0, state)
+      assert_receive {:ws_frame, _} = msg
+
+      assert {:ok, ^payload, state} = WS.handle_info(msg, state)
+    end
+
+    test "not send invalid data format via adapter", %{state: state} do
+      assert_raise Protocol.UndefinedError, fn ->
+        WS.send([list: :type, pid: self()], 0, state)
+      end
+    end
+  end
+end
